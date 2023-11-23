ce79845e2915ba860abc07744f85658f0b110fb7
richelbilderbeek <richel@richelbilderbeek.nl>
2023-11-23 11:09:45 +0100

    'add_numbers' must check for input

--- learners/jonrich/add_numbers.py
+++ learners/jonrich/add_numbers.py
@@ -4,3 +4,11 @@ def add_numbers(a, b):
 
 
 assert add_numbers.__doc__
+
+
+has_raised = False
+try:
+    add_numbers("evil string", "another evil")
+except AssertionError:
+    has_raised = True
+assert has_raised
