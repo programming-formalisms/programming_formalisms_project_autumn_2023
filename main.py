9a9d073c65e26d4e9b4a5b45b4b9dbc798d00dc8
richelbilderbeek <richel@richelbilderbeek.nl>
2023-11-27 06:47:55 +0100

    Can run main.py

--- main.py
+++ main.py
@@ -1,51 +1,22 @@
-"""The Programming Formalisms Example Project.
+"""The Programming Formalisms Learner Project.
 
-Project used in the UPPMAX Programming Formalisms course.
+Project used in the UPPMAX Programming Formalisms course
+in autumn 2023.
+
+Usage, from the root folder:
+
+python3 main.py
 """
 
-import sys
-
-from pf_example.simulation_controller import (
-    SimulationTerminalController,
-    SimulationWindowController,
+# Do not start with 'src.', 
+# install the package instead:
+#
+# python3 -m pip install .
+#
+# (in the root folder)
+from pfpa2023.testing import (
+    is_prime_richel,
 )
-
-"""
-TODO
-
-import cProfile
-
-from pf_example.testing import (
-    get_datas,
-    get_sorting_functions,
-    get_speed_measurements,
-    save_speed_measurements,
-)
-"""
 
 if __name__ == "__main__":
-
-    if "--gui" in sys.argv:
-        print("GUI application") # noqa: T201 print is used as a stub
-        c = SimulationWindowController()
-        c.run()
-    else:
-        print("Console application") # noqa: T201 print is used as a stub
-        c = SimulationTerminalController()
-
-
-"""
-TODO
-    speed_measurements = get_speed_measurements(
-        datas = get_datas(data_lengths = [2, 4, 6, 8, 10]),
-        functions = get_sorting_functions(),
-    )
-    save_speed_measurements(speed_measurements, "speeds.csv")
-
-    cProfile.run(
-        "get_speed_measurements("
-        "datas = get_datas(data_lengths = [2, 4, 6, 8, 10]), "
-        "functions = get_sorting_functions())",
-    )
-
-"""
+    print("13 is prime: ", is_prime_richel(13)) # noqa: T201 print is used as a stub
