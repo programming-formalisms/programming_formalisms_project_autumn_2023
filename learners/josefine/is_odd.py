27af4a85cfd7422e9ea2b4d2cd4fd4514dc814d1
johvan <johvan@kth.se>
2023-11-29 10:59:20 +0100

    typeerror test incomplete

--- learners/josefine/is_odd.py
+++ learners/josefine/is_odd.py
@@ -5,3 +5,11 @@ def is_odd(number):
 
 assert is_odd(1)
 assert is_odd(2) == False
+
+has_raised_typeerror = False
+try:
+    is_odd('string')
+except TypeError:
+    has_raised_typeerror = True
+assert has_raised_typeerror == False
+    
