c225f9ec81071d045eb7271eadd9769f6143edaa
AlexFHart <alex.f.hart@outlook.com>
2023-11-29 11:26:08 +0100

    string

--- learners/alex/is_odd.py
+++ learners/alex/is_odd.py
@@ -5,8 +5,13 @@ def is_odd(number):
 	test whether a number is odd or not
 	"""
 	
-	return number % 2 != 0
+	if isinstance(number, int):
+		return number % 2 != 0
+	else:
+		print("you goof'd it")
 
 assert is_odd(1) == True
 assert is_odd(2) == False
+assert is_odd("stringput") 
+
 
