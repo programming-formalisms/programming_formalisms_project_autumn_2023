2fcee17788f8b59bb5ec8a35cbc16da6c60e248a
ursdannenberg <ur.schaefer@live.de>
2023-11-29 10:22:19 +0100

    Account for non-integer input.

--- learners/urs/iseven.py
+++ learners/urs/iseven.py
@@ -1,7 +1,9 @@
 def is_even(number):
-
+    if not isinstance(number, int):
+        raise TypeError('number must be an integer!')
     return number % 2 == 0
 
 assert is_even(2)
 assert is_even(1)==False
-assert is_even("a")
+#assert is_even("a")
+assert is_even.__doc__
