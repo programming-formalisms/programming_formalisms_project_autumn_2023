d9355f3f96da4634069d7b1a086089aeba3c0069
Shimelis Abebe Tegegn <shulucy100@gmail.com>
2023-11-30 21:01:44 +0100

    Update test_environment.py
    Fixing lint

--- tests/test_environment.py
+++ tests/test_environment.py
@@ -5,8 +5,8 @@ from src.pf_example.enviro import Environment
 
 class TestEnvironment(unittest.TestCase):
 
-"""Class to test the code in src.pf2023_example.enviro."""
+    """Class to test the code in src.pf2023_example.enviro."""
   
-def test_can_create_params(self):
-    """#23: Can construct a Environment."""
-    Environment()
+    def test_can_create_params(self):
+       """#23: Can construct a Environment."""
+       Environment()
