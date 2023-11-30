833f6003a3d3b0513bbedda6079fe5f19417fccc
Shimelis Abebe Tegegn <shulucy100@gmail.com>
2023-11-30 20:39:59 +0100

    Update test_environment.py
    removing whites spaces

--- tests/test_environment.py
+++ tests/test_environment.py
@@ -5,10 +5,8 @@ from src.pf_example.enviro import Environment
 
 class TestEnvironment(unittest.TestCase):
 
-  """Class to test the code in src.pf2023_example.enviro."""
+"""Class to test the code in src.pf2023_example.enviro."""
   
-  def test_can_create_params(self):
+def test_can_create_params(self):
     """#23: Can construct a Environment."""
     Environment()
-   
-
