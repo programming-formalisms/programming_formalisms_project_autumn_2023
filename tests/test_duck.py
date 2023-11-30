54129a37cf9f334610b30223baed7d38c2c718c6
richelbilderbeek <richel@richelbilderbeek.nl>
2023-11-30 12:49:51 +0100

    Can create a Duck

---
+++ tests/test_duck.py
@@ -0,0 +1,15 @@
+"""Tests all function in src.pf_example.duck."""
+import unittest
+
+from src.pfpa2023.simulation_parameters import (
+    Duck,
+)
+
+
+class TestDuck(unittest.TestCase):
+
+    """Class to test the code in src.pfpa2023.duck."""
+
+    def test_can_create_duck(self):
+        """#14: Can construct a Duck."""
+        Duck()
