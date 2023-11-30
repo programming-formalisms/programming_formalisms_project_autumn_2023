0a0615eaa2020a9338b9f7ea4d1d8a04fe2dfb80
Shimelis Abebe Tegegn <shulucy100@gmail.com>
2023-11-30 20:16:01 +0100

    Update test_environment.py
    fixing blocks and space

--- tests/test_environment.py
+++ tests/test_environment.py
@@ -1,18 +1,14 @@
 """Tests all function in src.pf2023_example.enviro."""
 import unittest
 
-from src.pf_example.enviro import(
-    Environment,
-)
+from src.pf_example.enviro import Environment
 
 class TestEnvironment(unittest.TestCase):
 
   """Class to test the code in src.pf2023_example.enviro."""
-
   def test_can_create_params(self):
 
     """#23: Can construct a Environment."""
-
     Environment()
    
 
