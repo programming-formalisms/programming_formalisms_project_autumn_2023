cdaff968c8af8903a88e3d35c4dadf2052fbd1a6
Shimelis Abebe Tegegn <shulucy100@gmail.com>
2023-11-30 20:33:18 +0100

    Update test_environment.py
    Add one blank line after the class docstring

--- tests/test_environment.py
+++ tests/test_environment.py
@@ -6,8 +6,8 @@ from src.pf_example.enviro import Environment
 class TestEnvironment(unittest.TestCase):
 
   """Class to test the code in src.pf2023_example.enviro."""
+  
   def test_can_create_params(self):
-
     """#23: Can construct a Environment."""
     Environment()
    
