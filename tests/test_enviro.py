c0afbaa2775ae2abba656b55b7b04aff32a54772
rjd <jryan.daniels@gmail.com>
2023-11-30 15:58:19 +0200

    indent corrections

--- tests/test_enviro.py
+++ tests/test_enviro.py
@@ -10,5 +10,5 @@ class TestEnvironment(unittest.TestCase):
   """Class to test the code in src.pf_example.Environment."""
   def test_can_create_params(self):
 
-  """#14: Can construct a Environment."""
-  Environment()
+    """#14: Can construct a Environment."""
+    Environment()
