a698f808eafac7222f123422de7d48b9a7d6c114
Shimelis Abebe Tegegn <shulucy100@gmail.com>
2023-11-30 21:44:50 +0100

    Fix linting issues in environment.py

---
+++ src/pfpa2023/environment.py
@@ -0,0 +1,7 @@
+# Renaming  Environment.py from  enviro.py
+"""Simulation parameters Environment."""
+
+
+class Environment:
+
+    """Environment holds the simulation's parameters for Environment."""
