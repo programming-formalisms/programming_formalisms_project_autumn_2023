2e306b08729591e3b1d873fafbf9903325c38c9f
Shimelis Abebe Tegegn <shulucy100@gmail.com>
2023-11-30 21:25:29 +0100

    Update test_environment.py
    Fix linting errors in test_environment.py

--- tests/test_environment.py
+++ tests/test_environment.py
@@ -1,12 +1,10 @@
 """Tests all function in src.pf2023_example.enviro."""
 import unittest
-
 from src.pf_example.enviro import Environment
 
 class TestEnvironment(unittest.TestCase):
-
     """Class to test the code in src.pf2023_example.enviro."""
   
     def test_can_create_params(self):
-       """#23: Can construct a Environment."""
-       Environment()
+        """#23: Can construct a Environment."""
+        Environment()
