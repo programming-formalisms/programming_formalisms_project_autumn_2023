4921b059ae6a2de63a41e26a130b9493c2f4a141
jesvedberg <jesvedberg@users.noreply.github.com>
2023-11-30 13:43:37 +0100

    added MotionType class and test (#28)
    added MotionType class and test

---
+++ tests/test_motion_type.py
@@ -0,0 +1,15 @@
+"""Tests all function in src/pfpa2023/motion_type.py."""
+import unittest
+
+from src.pfpa2023.motion_type import (
+    MotionType,
+)
+
+
+class TestMotionType(unittest.TestCase):
+
+    """Class to test the code in src.pfpa2023.motion_type."""
+
+    def test_can_create_params(self):
+        """#[issue_number]: Can create MotionType."""
+        MotionType()
