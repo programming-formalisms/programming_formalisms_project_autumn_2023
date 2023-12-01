64166fd0932ab9f26903941c04c5a330178737fa
richelbilderbeek <richel@richelbilderbeek.nl>
2023-12-01 06:33:04 +0100

    Add benchmarking code

---
+++ benchmark.py
@@ -0,0 +1,34 @@
+"""Benchmarks code."""
+
+import cProfile
+import sys
+
+from pfpa2023.testing import (
+  is_prime_harald,
+  is_prime_jesper,
+  is_prime_pontus,
+  is_prime_richel,
+  is_prime_thanadol,
+)
+
+def run_benchmark():
+    """Put here the code you want to benchmark"""
+    for i in range(0, 100):
+        is_prime_harald(i)
+        is_prime_jesper(i)
+        is_prime_pontus(i)
+        is_prime_richel(i)
+        is_prime_thanadol(i)
+
+
+def do_benchmark():
+    """Benchmark this project."""
+    cProfile.run(
+        "run_benchmark()"
+    )
+
+if __debug__:
+    msg = "Do not benchmark in debug mode. Tip: run 'python -O profiling.py''"
+    e = RuntimeError(msg)
+    raise e
+do_benchmark()
