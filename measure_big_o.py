ca363678d7c96b4638ad3d47193c264a22105e98
richelbilderbeek <richel@richelbilderbeek.nl>
2023-12-01 14:15:07 +0100

    Benchmark for Richel

--- measure_big_o.py
+++ measure_big_o.py
@@ -4,19 +4,11 @@
 # Functions are adapted as little as possible :-)
 #
 def isprime_1(num):
-  for n in range(
-    2, int(num**0.5)+1
-  ):
-    if num%n==0:
-      return False
-  return True
+  return all(num % n != 0 for n in range(2, int(num ** 0.5) + 1))
 
 def isprime_2(num):
-    if num> 1:  
-        for n in range(2,num):  
-            if (num % n) == 0:  
-                return False
-        return True
+    if num> 1:
+        return all(num % n != 0 for n in range(2, num))
     else:
         return False
 
@@ -37,12 +29,10 @@ def is_prime_3(no, i = 2):
 # Richel's code
 #################################################
 def measure_big_o_richel():
-    times_1 = [] # For is_prime_1
-    times_2 = [] # For is_prime_2
-    times_3 = [] # For is_prime_3
+    pass # For is_prime_2
     # Make i go from 1 to 1000 (if that is
         # Measure the time for each of the prime functions
         # Store the time it took
-    # Show all the times, ideally in a plot    
+    # Show all the times, ideally in a plot
 
 
